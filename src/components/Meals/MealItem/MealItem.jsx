4e1bdc492449147490757d5d0e27e0313056f888
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-12 15:42:09 +0530

    MealItem Form Component

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
   const price = `₹${props.price}`;
@@ -11,7 +12,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
--- src/components/Meals/MealsSummery.jsx
+++ src/components/Meals/MealsSummery.jsx
@@ -7,14 +7,13 @@ const MealsSummery = () => {
     <section className={classes.summary}>
       <h2>Deliciousness delivered to your doorstep.</h2>
       <p>
-        We have a wide range of options to cater to different tastes. Enjoy
-        juicy steaks cooked to perfection, tender grilled chicken, flavorful
-        seafood dishes like grilled salmon or shrimp scampi, and creamy
-        fettuccine Alfredo or spicy marinara with meatballs.
+        We’re craft Delicious Foods made in-house. Get traditions and favorites
+        from all over the world — all on one plate.
       </p>
       <p>
-        Choose your favourite meal from our broad selection of available meals
-        and enjoy lunch or dinner at home.
+        Everything we serve is house-made and prepared by a staff that cares.
+        You could spend a lifetime sampling every flavor combination at Spice
+        Route, and we hope you do.
       </p>
     </section>
   );
