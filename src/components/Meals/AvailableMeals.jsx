f0684be43bdeac98781e609da51c2d3b79fe8e2a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-05-19 22:10:33 +0530

    Fetching Meals Via Firebase DB

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,51 +1,32 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Chicken Snack Meal',
-    description: '2 Pcs Chicken + 3 Strips',
-    price: 369,
-  },
-  {
-    id: 'm2',
-    name: '10 Pc Grilled Chicken',
-    description: '10 Pc Grilled Chicken',
-    price: 849,
-  },
-  {
-    id: 'm3',
-    name: 'M Crunchy Burger',
-    description: 'Crispy And Spicy Chicken Burger',
-    price: 149,
-  },
-  {
-    id: 'm4',
-    name: 'Burger Meal',
-    description: '1 Pc Royal Crunchy Burger + Fries + Drink 330 Ml',
-    price: 189,
-  },
-  {
-    id: 'm5',
-    name: 'Bbq Chicken Pizza',
-    description:
-      'Bbq Chciken Pizza - Made With Bbq Sauce,Red Onionand With Mozzarella Cheese',
-    price: 289,
-  },
-  {
-    id: 'm6',
-    name: 'Mexicano Pizza',
-    description: 'Mexicano - Classic Sauce Topped With Mozzarella Cheese',
-    price: 299,
-  },
-];
-
 export const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(process.env.REACT_APP_FIREBASE_DB);
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
